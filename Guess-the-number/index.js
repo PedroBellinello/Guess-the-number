d6859daed48a4369bfadffee1aeedb9f9b9e33bd
PedroBellinello <107586888+PedroBellinello@users.noreply.github.com>
2022-10-25 17:13:06 -0300

    Add files via upload

---
+++ Guess-the-number/index.js
@@ -0,0 +1,50 @@
+
+let computerNumber
+let userNumbers = [] 
+let attempts = 0
+let maxguesses = 10
+
+//resetGame
+
+function newGame(){
+    window.location.reload()
+}
+
+//selecionar um numero aleatorio
+function init() {
+    computerNumber = Math.floor(Math.random() * 100 + 1)
+    console.log(computerNumber)
+};
+
+
+//pegar o valor do input / comparar / registrar tentativas
+  function compareNumbers() {
+   const userNumber = Number(document.getElementById("inputBox").value)
+   userNumbers.push(" " + userNumber)
+   document.getElementById("guesses").innerHTML = userNumbers
+
+    if(attempts < maxguesses) {
+     if (userNumber > computerNumber) {
+         document.getElementById("textOutput").innerHTML = "Your number is too high"
+         document.getElementById("inputBox").value = ""
+         attempts++
+         document.getElementById("attempts"). innerHTML = attempts
+     }
+     else if (userNumber < computerNumber) {
+         document.getElementById("textOutput").innerHTML = "Your number is too low"
+         document.getElementById("inputBox").value = ""
+         attempts++
+         document.getElementById("attempts"). innerHTML = attempts
+     }
+     else {
+        document.getElementById("textOutput").innerHTML = "Congratulations you win!!" 
+        attempts++
+        document.getElementById("attempts"). innerHTML = attempts
+        document.getElementById("inputBox").setAttribute("Readonly", "Readonly")
+     };
+   }else {
+        document.getElementById("textOutput").innerHTML = "You lose!! the computer number was: "  + computerNumber  
+        document.getElementById("inputBox").setAttribute("Readonly", "Readonly")
+   };
+  };
+
